7a7e1715ce87724b4411109869ef0fae4334255e
Ross Morgan <ross.morgan933@gmail.com>
2024-12-07 11:53:14 -0600

    exercise cards are rendering

---
+++ components/cards/ExerciseCard.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// eslint-disable-next-line import/no-duplicates
+import Button from 'react-bootstrap/card';
+// eslint-disable-next-line import/no-duplicates
+import Card from 'react-bootstrap/card';
+import Link from 'next/link';
+import { deleteExercise } from '../../api/exerciseData';
+import { useAuth } from '../../utils/context/authContext';
+
+function ExerciseCard({ exerciseObj, onUpdate }) {
+  const { user } = useAuth();
+
+  // * for deleteing events
+  const deleteThisExercise = () => {
+    if (window.confirm(`Delete ${exerciseObj.exerciseName}?`)) {
+      deleteExercise(exerciseObj.id).then(() => onUpdate());
+    }
+  };
+  console.warn(exerciseObj);
+  console.warn(user);
+  const isOwner = exerciseObj.userId === user.id;
+
+  return (
+    <Card id="card" style={{ width: '18rem', margin: '10px' }}>
+      <Card.Img variant="top" src={exerciseObj.imageUrl} alt={exerciseObj.exerciseName} style={{ height: '400px' }} />
+      <Card.Body>
+        <Card.Title>
+          {exerciseObj.exerciseName}
+          <br />
+        </Card.Title>
+        <p className="card-text bold">
+          <br />
+          <div>
+            <Card.Text>
+              {exerciseObj.exerciseTag?.map((tag) => (
+                <span key={tag.tag.id}>{tag.tag.name} </span>
+              ))}
+            </Card.Text>
+          </div>
+        </p>
+        <br />
+        {isOwner && (
+        <Link href={`/exercise/edit/${exerciseObj.id}`} passHref>
+          <Button style={{ background: '#008000', border: 'solid 1px black' }} variant="info">EDIT</Button>
+        </Link>
+        )}
+        {isOwner && (
+        <Button variant="danger" style={{ background: '#8b0000', border: 'solid 1px black' }} onClick={deleteThisExercise} className="m-2">
+          DELETE
+        </Button>
+        )}
+      </Card.Body>
+    </Card>
+  );
+}
+
+ExerciseCard.propTypes = {
+  exerciseObj: PropTypes.shape({
+    exerciseName: PropTypes.string,
+    id: PropTypes.number,
+    uid: PropTypes.string,
+    userId: PropTypes.number,
+    imageUrl: PropTypes.string,
+    exerciseTag: PropTypes.arrayOf(PropTypes.shape({
+      tag: PropTypes.shape({
+        name: PropTypes.string,
+        id: PropTypes.number,
+      }),
+    })),
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
+
+export default ExerciseCard;
---
+++ pages/exercise/exercisePage.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+
+// eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
+
+import { getAllExercises } from '../../api/exerciseData';
+import ExerciseCard from '../../components/cards/ExerciseCard';
+
+export default function VenuesPage() {
+  // *set state for venues
+  const [exercises, setExercises] = useState([]);
+
+  // function to get all venues
+  const getAllTheExercises = () => {
+    getAllExercises().then(setExercises);
+  };
+
+  // make api call to get all venues
+  useEffect(() => {
+    getAllTheExercises();
+  }, []);
+
+  return (
+    <div className="text-center my-4">
+      <Link href="/venues/edit/new" passHref>
+        <Button>Create Exercise</Button>
+      </Link>
+      <div className="d-flex flex-wrap">{exercises.map((exercise) => <ExerciseCard key={exercise.id} exerciseObj={exercise} onUpdate={getAllTheExercises} />)}</div>
+    </div>
+  );
+}
